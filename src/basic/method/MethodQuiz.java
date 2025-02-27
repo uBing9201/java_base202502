010677d6cc3e1bca446c8e13b7cf2b48c6dace97
ubing <ubing0101@kakao.com>
2025-02-27 10:43:27 +0900

    Day05 - 메서드 퀴즈 풀이

--- src/basic/method/MethodQuiz.java
+++ src/basic/method/MethodQuiz.java
@@ -23,109 +23,129 @@ public class MethodQuiz {
     }
 
     // 특정 요소가 배열에 포함되어 있는지의 여부를 확인하는 메서드를 작성하세요.
-    // static boolean isInclude(String searchTarget)
     static boolean isInclude(String searchTarget) {
-
-        int i = 0;
-
-        while (i < foods.length) {
-            if(foods[i].equals(searchTarget)) {
-                return true;
-            }
-            i++;
-        }
-
-        return false;
+//        int idx = indexOf(searchTarget);
+//        if (idx == -1) return false;
+//        else return true;
+        return indexOf(searchTarget) != -1;
     }
+
 
     // foods 배열에 데이터를 끝에 추가하는 메서드를 작성하세요.
-    // static void push(String newFood)
     static void push(String newFood) {
-        String[] newFoods = new String[foods.length + 1];
-        for (int i = 0; i < foods.length; i++) {
-            newFoods[i] = foods[i];
-        }
-        newFoods[foods.length] = newFood;
-        foods = newFoods;
+        String[] temp = copy(1);
+        temp[temp.length - 1] = newFood;
+        foods = temp;
+        temp = null;
     }
+
+    // 원본 배열의 값을 새 배열로 복사하는 메서드
+    static String[] copy(int size) {
+        // 원본 배열의 값을 복사하는 행위는 새 배열을 만들어서 진행하는 것이기 때문에
+        // copy에서 새 배열을 만드는 것을 대신 해 주자.
+        String[] temp = makeNewArray(size);
+
+        // 반복문의 횟수를 size로 판단해서 상황에 따라 다르게 지정.
+        // size가 0보다 작다? -> temp가 foods보다 더 작다 -> 끝범위는 temp.length
+        // size가 0보다 크다? -> temp가 foods보다 더 크다 -> 끝범위는 foods.length
+        int loopCount = size >= 0 ? foods.length : temp.length;
+        for (int i = 0; i < loopCount; i++) {
+            temp[i] = foods[i];
+        }
+
+        return temp;
+    }
+
 
     // 특정 인덱스가 범위 내의 올바른 인덱스인지를 확인하는 메서드를 작성하세요.
-    // static boolean isOutOfBounds(int targetIndex)
     // 작성이 완료되면 타 메서드에서 활용할 수 있을 것입니다.
     static boolean isOutOfBounds(int targetIndex) {
-        return targetIndex < 0 || targetIndex >= foods.length;
+        return targetIndex > foods.length - 1 || targetIndex < 0;
+//        if (targetIndex < 0 || targetIndex > foods.length - 1) {
+//            return true;
+//        }
+//        return false;
     }
+
 
     // 원하는 인덱스 위치에 요소를 삽입하는 메서드를 작성하세요.
-    // static void insert(int targetIndex, String newFoodName)
     // targetIndex는 기존 foods의 배열 인덱스 범위 내여야 합니다.
     static void insert(int targetIndex, String newFoodName) {
-        if (isOutOfBounds(targetIndex)) {
-            System.out.println("배열 인덱스범위가 아닙니다.");
-            return;
+        if (isOutOfBounds(targetIndex)) return;
+
+        String[] temp = copy(1);
+
+        for (int i = foods.length; i > targetIndex; i--) {
+            temp[i] = temp[i - 1];
         }
-        String[] newFoods = new String[foods.length + 1];
-        for(int i = 0, j = 0; i < newFoods.length; i++) {
-            if(i == targetIndex) {
-                newFoods[i] = newFoodName;
-            }else{
-                newFoods[i] = foods[j++];
-            }
-        }
-        foods = newFoods;
+
+        temp[targetIndex] = newFoodName;
+        foods = temp;
     }
 
+    // 기존 배열 대비 크기가 다른 새 배열을 리턴해 주는 메서드 (temp 만드는 것 메서드화)
+    // size에 따라 크기가 큰 or 크기가 작은 배열을 리턴.
+    static String[] makeNewArray(int size) {
+        return new String[foods.length + size];
+    }
+
+
     // 원하는 인덱스 위치의 요소를 수정하는 메서드를 작성하세요.
-    // void modify(int targetIndex, String newFoodName)
     // targetIndex는 기존 foods의 배열 인덱스 범위 내여야 합니다.
     static void modify(int targetIndex, String newFoodName) {
-        if (isOutOfBounds(targetIndex)) {
-            System.out.println("배열 인덱스 범위가 아닙니다.");
-            return;
-        }
+        if (isOutOfBounds(targetIndex)) return;
         foods[targetIndex] = newFoodName;
     }
 
+
     // 특정 인덱스의 요소를 삭제하는 메서드를 작성하세요.
-    // void remove(String deleteTarget)
     static void remove(String deleteTarget) {
-        int idx = indexOf(deleteTarget);
-        if (idx == -1) {
-            System.out.println("해당 음식이 존재하지 않습니다.");
-            return;
+        int index = indexOf(deleteTarget);
+        if (index == -1) return;
+
+        // copy를 호출하기 전에 지우고자 하는 위치를 기준으로
+        // 뒤에 있는 값을 한 칸씩 앞으로 땡겨야 한다.
+        for (int i = index; i < foods.length-1; i++) {
+            foods[i] = foods[i + 1];
         }
-        String[] newFoods = new String[foods.length - 1];
-        for (int i = 0, j = 0; i < foods.length; i++) {
-            if (i != idx) {
-                newFoods[j++] = foods[i];
-            }
-        }
-        foods = newFoods;
+
+        foods = copy(-1);
+
     }
 
+
     // 배열 내부를 깔끔하게 비워버리는 메서드를 작성하세요. (삭제 반복 x)
-    // void clear()
     static void clear() {
-        foods= new String[0];
+        foods = new String[] {};
     }
 
 
     public static void main(String[] args) {
 
-//        printFoods();
-//        int idx = indexOf("apple");
-//        System.out.println("idx = " + idx);
+        printFoods();
+        int idx = indexOf("apple");
+        System.out.println("idx = " + idx);
 
+        boolean flag = isInclude("pork");
+        System.out.println("flag = " + flag);
+
+        push("pork");
+        push("icecream");
         printFoods();
-        push("pizza");
+
+        insert(2, "apple");
         printFoods();
-        insert(1, "sushi");
+
+        modify(1, "hamburger");
         printFoods();
-        modify(2, "burger");
+
+        remove("beef");
         printFoods();
-        remove("pasta");
-        printFoods();
+
         clear();
+        printFoods();
+
+        push("banana");
         printFoods();
 
     }
