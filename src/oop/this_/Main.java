2058129cf6afa3ac9d8cfcda9c3c28192ac95cba
ubing <ubing0101@kakao.com>
2025-02-27 15:29:45 +0900

    Day05 - this

--- src/oop/this_/Main.java
+++ src/oop/this_/Main.java
@@ -5,13 +5,19 @@ public class Main {
     public static void main(String[] args) {
 
         Player p1 = new Player("플레이어1");
-        System.out.println("p1이 띠는 주소값: " + p1);
-//        System.out.println(p1.toString());
+        System.out.println(p1.toString());
 
         System.out.println("====================");
 
         Player p2 = new Player("플레이어2");
-        System.out.println("p2이 띠는 주소값: " + p2);
+        System.out.println(p2);
+
+        p1.attack(p1);
+
+        System.out.println("====================");
+
+        Player p3 = new Player("플레이어3", 1000);
+        System.out.println(p3);
     }
 
 }
--- src/oop/this_/Player.java
+++ src/oop/this_/Player.java
@@ -8,25 +8,48 @@ public class Player {
     int hp;
 
     Player() {
-        System.out.println("Player의 기본 생성자 호출!");
+        System.out.println("기본 생성자.");
+        nickName = "";
         level = 1;
         hp = 50;
     }
 
     Player(String nickName) {
-        System.out.println("Player의 2번 생성자 호출!");
-        System.out.println("this가 띠는 주소값: " + this);
+        this();
+        System.out.println("2번 생성자");
         this.nickName = nickName;
-        this.level = 1;
-        this.hp = 50;
     }
 
-//    @Override
-//    public String toString() {
-//        return "Player{" +
-//                "nickName='" + nickName + '\'' +
-//                ", level=" + level +
-//                ", hp=" + hp +
-//                '}';
-//    }
+    Player(String nickName, int hp) {
+        this(nickName);
+        System.out.println("3번 생성자");
+        this.hp = hp;
+    }
+
+    // 일반 메서드
+    void attack(Player target) {
+        if(target == this) {
+            System.out.println("타겟이 잘못되었습니다.");
+            return;
+        }
+        System.out.printf("%s이(가) %s을(를) 공격합니다\n", this.nickName, target.nickName);
+
+        // 맞는 Player의 체력을 10 감소시키자.
+        target.hp -= 10;
+
+        // 때리는 Player의 체력은 3 감소시키자.
+        this.hp -= 3;
+
+        System.out.printf("%s님의 남은체력: %d\n",this.nickName, this.hp);
+        System.out.printf("%s님의 남은체력: %d\n",target.nickName, target.hp);
+    }
+
+    @Override
+    public String toString() {
+        return "Player{" +
+                "nickName='" + nickName + '\'' +
+                ", level=" + level +
+                ", hp=" + hp +
+                '}';
+    }
 }
