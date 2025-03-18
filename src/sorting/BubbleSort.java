668dc7e4924c850903b7896e7aca558767d03ee3
ubing <ubing0101@kakao.com>
2025-03-18 15:02:31 +0900

    정렬 자료구조

---
+++ src/sorting/BubbleSort.java
@@ -0,0 +1,24 @@
+package sorting;
+
+import java.util.Arrays;
+
+public class BubbleSort {
+
+    public static void main(String[] args) {
+
+        int[] arr = {42, 32, 24, 60, 15};
+
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr.length - 1; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+        }
+
+        System.out.println(Arrays.toString(arr));
+    }
+
+}
---
+++ src/sorting/InsertionSort.java
@@ -0,0 +1,21 @@
+package sorting;
+
+public class InsertionSort {
+
+    public static void main(String[] args) {
+
+        int[] arr = {42, 32, 24, 60, 15};
+
+        int n = arr.length;
+
+        for (int i = 1; i < n; i++) {
+            int temp = arr[i];
+            int j = i - 1;
+            while (j >= 0 && arr[j] > temp) {
+
+            }
+        }
+
+    }
+
+}
